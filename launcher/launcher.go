d951a56ef102dec5374b584b835629da60495764
Rafael Leite <rafael.leite@tfgco.com>
2018-06-29 12:41:29 -0300

    Exit with status 1 on bot execution error

--- launcher/launcher.go
+++ launcher/launcher.go
@@ -40,16 +40,27 @@ func Launch(config *viper.Viper, specPath string) {
 
 	logger.Infof("Launching %d bots\n", spec.NumberOfInstances)
 	var wg sync.WaitGroup
+	var boterr []error
+	var errmutex sync.Mutex
 	for i := 0; i < spec.NumberOfInstances; i++ {
 		wg.Add(1)
 		go func(i int) {
 			if err := runner.Run(config, spec, i); err != nil {
 				logger.Error("Bot execution failed")
 				logger.Error(err)
+
+				errmutex.Lock()
+				boterr = append(boterr, err)
+				errmutex.Unlock()
 			}
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
+
+	if len(boterr) > 0 {
+		logger.Error(boterr)
+		os.Exit(1)
+	}
 	logger.Info("Finished running bots")
 }
