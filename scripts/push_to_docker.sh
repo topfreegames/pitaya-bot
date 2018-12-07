b0671e034ca356eaef3044bbdecf172be8f431ea
Rodrigo Chen <rodrigo.kashiwakura@tfgco.com>
2018-12-07 14:36:51 -0200

    Build only when is not a pr anymore, is already merged

--- scripts/push_to_docker.sh
+++ scripts/push_to_docker.sh
@@ -3,19 +3,21 @@
 COMMIT=$TRAVIS_COMMIT
 TAG=$TRAVIS_BUILD_NUMBER-${COMMIT:0:7}
 
-docker build -t pitaya-bot .
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    docker build -t pitaya-bot .
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
-docker tag pitaya-bot:latest tfgco/pitaya-bot:$TAG
-docker push tfgco/pitaya-bot:$TAG
-docker tag pitaya-bot:latest tfgco/pitaya-bot:latest
-docker push tfgco/pitaya-bot
+    docker tag pitaya-bot:latest tfgco/pitaya-bot:$TAG
+    docker push tfgco/pitaya-bot:$TAG
+    docker tag pitaya-bot:latest tfgco/pitaya-bot:latest
+    docker push tfgco/pitaya-bot
 
-DOCKERHUB_LATEST=$(python ./scripts/get_latest_tag.py)
+    DOCKERHUB_LATEST=$(python ./scripts/get_latest_tag.py)
 
-if [ "$DOCKERHUB_LATEST" != "$TAG" ]; then
-    echo "Last version is not in docker hub!"
-    echo "docker hub: $DOCKERHUB_LATEST, expected: $TAG"
-    exit 1
+    if [ "$DOCKERHUB_LATEST" != "$TAG" ]; then
+        echo "Last version is not in docker hub!"
+        echo "docker hub: $DOCKERHUB_LATEST, expected: $TAG"
+        exit 1
+    fi
 fi
