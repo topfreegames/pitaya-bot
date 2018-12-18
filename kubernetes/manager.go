d466be7a1bb49adc591bc8f3fe2cb33c094dd081
Rodrigo Chen <rodrigo.kashiwakura@tfgco.com>
2018-12-18 17:40:24 -0200

    made the resources be equal 250m CPU and 256 MB of RAM

--- kubernetes/manager.go
+++ kubernetes/manager.go
@@ -11,6 +11,7 @@ import (
 	"github.com/topfreegames/pitaya-bot/models"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -61,6 +62,16 @@ func CreateManagerPod(logger logrus.FieldLogger, clientset kubernetes.Interface,
 						{
 							Name:      "manager-config",
 							MountPath: "/etc/pitaya-bot",
+						},
+					},
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.Quantity{Format: "250m"},
+							corev1.ResourceMemory: resource.Quantity{Format: "256Mi"},
+						},
+						Limits: corev1.ResourceList{
+							corev1.ResourceCPU:    resource.Quantity{Format: "250m"},
+							corev1.ResourceMemory: resource.Quantity{Format: "256Mi"},
 						},
 					},
 					Command: []string{"./main"},
@@ -149,6 +160,16 @@ func DeployJobs(logger logrus.FieldLogger, clientset kubernetes.Interface, confi
 									{
 										Name:      "config",
 										MountPath: "/etc/pitaya-bot",
+									},
+								},
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.Quantity{Format: "250m"},
+										corev1.ResourceMemory: resource.Quantity{Format: "256Mi"},
+									},
+									Limits: corev1.ResourceList{
+										corev1.ResourceCPU:    resource.Quantity{Format: "250m"},
+										corev1.ResourceMemory: resource.Quantity{Format: "256Mi"},
 									},
 								},
 								Command: []string{"./main"},
